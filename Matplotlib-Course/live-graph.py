7d441169247a3c560201ceacc1d6d3cdf5e25917
hasanarcas <hasanarcas@gmail.com>
2022-10-10 22:47:35 +0300

    Added more matplotlib material

---
+++ Matplotlib-Course/live-graph.py
@@ -0,0 +1,24 @@
+from matplotlib.pyplot import style
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+style.use('fivethirtyeight')
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+    graph_data = open('samplefile.txt', 'r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line)>1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
+    ax1.clear()
+    ax1.plot(xs, ys)
+    
+ani = animation.FuncAnimation(fig, animate, interval=1000)
+plt.show()
